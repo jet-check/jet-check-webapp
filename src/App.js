ba2c6a0b0fe9f8014ffe1f6068293f13aaa0f3f7
oujianzhang <51085845+oujianzhang@users.noreply.github.com>
2020-11-22 22:56:39 +0100

    Passworteingabe fertig

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { JetCheckNavbar } from "./components/JetCheckNavbar";
+import { Passworteingabe } from "./components/Passworteingabe";
+
 const App = () => {
+  const [modalShow, setModalShow] = React.useState(false);
+  
   return (
     <div className="container">
       <JetCheckNavbar />
---
+++ src/components/Passworteingabe.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
+import { Modal } from "react-bootstrap";
+
+export default function Passworteingabe() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Passworteingabe">
+      <Form onSubmit={handleSubmit}>
+        <Modal
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Passworteingabe
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group size="lg" controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                autoFocus
+                type="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={props.onHide}>Abbrechen</Button>
+            <Button block size="lg" type="submit" disabled={!validateForm()}>
+              Login
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Form>
+    </div>
+  );
+}
