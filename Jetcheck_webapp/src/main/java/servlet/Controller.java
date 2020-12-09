e368bbe0abc71c84d0eb15a788439ad1166c3c3d
Matio <matiotawdrous@gmail.com>
2020-12-09 22:57:26 +0100

    db classes vorbereitet, fehlt nur button zu controller

--- Jetcheck_webapp/src/main/java/servlet/Controller.java
+++ Jetcheck_webapp/src/main/java/servlet/Controller.java
@@ -4,9 +4,16 @@ import beans.Ware;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +26,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "Controller", urlPatterns = {"/Controller"})
 public class Controller extends HttpServlet {
-    private String warenname;
+    private String WarenName;
+    private Connection connection;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -32,7 +40,7 @@ public class Controller extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        warenname = request.getParameter("warenname");
+        WarenName = request.getParameter("warenname");
         request.getRequestDispatcher("WareSubmenu.jsp").forward(request, response);
     }
 
@@ -54,26 +62,51 @@ public class Controller extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+        connect();
+    }
+    
+    public void connect () {
         try {
             Class.forName("org.postgresql.Driver");
             String url = "jdbc:postgresql://localhost:5432/jet-check";
             String username = "postgres";
             String password = "postgresql";
-            Connection connection = DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connection established!");
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println(ex.toString());
         }
     }
-
-    public void insertWare() {
+    
+    public void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+    
+    public void insertWare() throws SQLException {
         List<Ware> warenliste = new ArrayList<>();
-        //String getWarenSqlString
+        String getWarenSqlString = "SELECT * FROM Ware";
+        Statement stat = connection.createStatement();
+        ResultSet rs = stat.executeQuery(getWarenSqlString);
+        while (rs.next()) {
+            String warenname = rs.getString("Warenname");
+            warenliste.add(new Ware(warenname));
+        }
+        
+        disconnect();
+        connect();
+        
+        String insertWareSqlString = "INSERT INTO Ware (warenname) + VALUES (?);";
+        PreparedStatement pStat = connection.prepareStatement(insertWareSqlString);
+        pStat.setString(1, WarenName);
+        pStat.executeUpdate();
     }
     
     /**
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
