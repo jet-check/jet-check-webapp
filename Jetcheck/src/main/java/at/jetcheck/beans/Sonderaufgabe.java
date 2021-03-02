c2d2adfcbe400e60bf20aad5036f9a963b3cd9eb
Matio <matiotawdrous@gmail.com>
2021-03-02 15:44:18 +0100

    Sonderaufgaben beans und DB_Access

---
+++ Jetcheck/src/main/java/at/jetcheck/beans/Sonderaufgabe.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.jetcheck.beans;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Matio Tawdrous
+ */
+public class Sonderaufgabe {
+    private String beschreibung;
+    private LocalDate datum;
+    private String Mitarbeiter;
+    private String name;
+    private String id;
+}
