a55448ea018ceea1cdda02418bc55ea257ce89d8
Matio <matiotawdrous@gmail.com>
2021-03-02 15:44:43 +0100

    beans get set con

--- Jetcheck/src/main/java/at/jetcheck/beans/Sonderaufgabe.java
+++ Jetcheck/src/main/java/at/jetcheck/beans/Sonderaufgabe.java
@@ -17,4 +17,55 @@ public class Sonderaufgabe {
     private String Mitarbeiter;
     private String name;
     private String id;
+
+    public Sonderaufgabe(String beschreibung, LocalDate datum, String Mitarbeiter, String name, String id) {
+        this.beschreibung = beschreibung;
+        this.datum = datum;
+        this.Mitarbeiter = Mitarbeiter;
+        this.name = name;
+        this.id = id;
+    }
+
+    public Sonderaufgabe() {
+    }
+    
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public LocalDate getDatum() {
+        return datum;
+    }
+
+    public void setDatum(LocalDate datum) {
+        this.datum = datum;
+    }
+
+    public String getMitarbeiter() {
+        return Mitarbeiter;
+    }
+
+    public void setMitarbeiter(String Mitarbeiter) {
+        this.Mitarbeiter = Mitarbeiter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
