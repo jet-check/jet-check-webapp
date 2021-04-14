5d814de0ed8a5f42b13f3637ab5d528105291612
Matio <matiotawdrous@gmail.com>
2021-04-14 22:08:58 +0200

    IO_Access Methoden zum bearbeiten der Dateien (Does not work till now)

--- Jetcheck/src/main/java/at/jetcheck/io/IO_Access.java
+++ Jetcheck/src/main/java/at/jetcheck/io/IO_Access.java
@@ -6,9 +6,11 @@
 package at.jetcheck.io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,5 +34,32 @@ public class IO_Access {
         BufferedReader br = new BufferedReader(new FileReader(new File(path)));
         List<String> aufgaben = br.lines().collect(Collectors.toList());
         return aufgaben;
+    }
+    public boolean editFruehaufgaben(String text, String path) throws IOException {
+        List<String> fruehaufgabn_old = getFruehaufgaben(path);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path)))) {
+            bw.write(text);
+        }
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        List<String> fruehaufgaben_new = br.lines().collect(Collectors.toList());
+        return fruehaufgaben_new.size() > fruehaufgabn_old.size() && fruehaufgaben_new.containsAll(fruehaufgabn_old);
+    }
+    public boolean editZwischenaufgaben(String text, String path) throws IOException {
+        List<String> zwischenaufgaben_old = getFruehaufgaben(path);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path)))) {
+            bw.write(text);
+        }
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        List<String> zwischenaufgaben_new = br.lines().collect(Collectors.toList());
+        return zwischenaufgaben_new.size() > zwischenaufgaben_old.size() && zwischenaufgaben_new.containsAll(zwischenaufgaben_old);
+    }
+    public boolean editSpaetaufgaben(String text, String path) throws IOException {
+        List<String> spaetaufgaben_old = getFruehaufgaben(path);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path)))) {
+            bw.write(text);
+        }
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        List<String> spaetaufgaben_new = br.lines().collect(Collectors.toList());
+        return spaetaufgaben_new.size() > spaetaufgaben_old.size() && spaetaufgaben_new.containsAll(spaetaufgaben_old);
     }
 }
