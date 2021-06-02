7e2d43716971e0fa6b3dc00ab0ae2e8fcb66cd7f
oujianzhang <51085845+oujianzhang@users.noreply.github.com>
2021-06-03 00:01:37 +0200

    bruchwaren pfad update

--- Jetcheck/src/main/java/at/jetcheck/bl/PDF_Converter.java
+++ Jetcheck/src/main/java/at/jetcheck/bl/PDF_Converter.java
@@ -28,7 +28,7 @@ public class PDF_Converter {
     
     public void exportBruchwarenPdf(List<Bruchware> bruchwaren, String von, String bis) throws FileNotFoundException {
         // Must have write permissions to the path folder
-        PdfWriter writer = new PdfWriter(System.getProperty("user.home") + "Bruchwaren_von_" + von + "_bis_" + bis + ".pdf");
+        PdfWriter writer = new PdfWriter(System.getProperty("user.home") + "\\Downloads\\Bruchwaren_von_" + von + "_bis_" + bis + ".pdf");
         PdfDocument pdf = new PdfDocument(writer);
         Document document = new Document(pdf);
         Paragraph header = new Paragraph("BRUCHWAREN von" + von + " bis " + bis).setTextAlignment(TextAlignment.CENTER).setFontSize(20);
